b5e2ae4720c635f7914923b43197f89bbe3d6610
mbarq <mbarquero90@gmail.com>
2018-02-10 15:43:07 -0800

    Audio initial commit

---
+++ Assets/rocket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+   
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        processInput();
+
+		
+	}
+
+    private void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
